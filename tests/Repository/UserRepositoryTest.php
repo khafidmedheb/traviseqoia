23f51571b9d802e5c7d04c240543c9858a7de90c
Khalid HAFID-MEDHEB <khalid.hafid-medheb@integragen.com>
2018-08-22 15:39:52 +0200

    test

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -20,106 +20,106 @@ class UserRepositoryTest extends KernelTestCase
 	/**
 	 * @var \Doctrine\ORM\EntityManager
 	 */
-	private $entityManager;
+	// private $entityManager;
 
 	/**
 	 * {@inheritDoc}
 	 */
-	protected function setUp()
-	{
-		$kernel = self::bootKernel();
+	// protected function setUp()
+	// {
+	// 	$kernel = self::bootKernel();
 
-		$this->entityManager = $kernel->getContainer()
-		    ->get('doctrine')
-		    ->getManager();
-	}
+	// 	$this->entityManager = $kernel->getContainer()
+	// 	    ->get('doctrine')
+	// 	    ->getManager();
+	// }
 
 	/**
 	 * Recherche si user10 existe dans users
 	 *
 	 */
-	public function testFindUserTen()
-	{
-		// Commande : ./vendor/bin/simple-phpunit --filter=testFindUserToto
+	// public function testFindUserTen()
+	// {
+	// 	// Commande : ./vendor/bin/simple-phpunit --filter=testFindUserToto
 
-		$resultExpected = "user10";
+	// 	$resultExpected = "user10";
 
-		$users = $this->entityManager
-		    ->getRepository(User::class)
-		    ->getUserToto($resultExpected);
+	// 	$users = $this->entityManager
+	// 	    ->getRepository(User::class)
+	// 	    ->getUserToto($resultExpected);
 
-		$this->assertEquals($resultExpected, $users);
-	}
+	// 	$this->assertEquals($resultExpected, $users);
+	// }
 
     /**
      * Recherche si le nomnre de users correspond à un nombre attendu
      * A utiliser avec les loadfixtures
      */
-    public function testFindAllUsers()
-    {
-    	// Commande : ./vendor/bin/simple-phpunit --filter=testFindAllUsers
+    // public function testFindAllUsers()
+    // {
+    // 	// Commande : ./vendor/bin/simple-phpunit --filter=testFindAllUsers
     	
-    	$resultExpected = 20;
+    // 	$resultExpected = 20;
 
-    	$users = $this->entityManager
-    	    ->getRepository(User::class)
-    	    ->findAllUsers();
+    // 	$users = $this->entityManager
+    // 	    ->getRepository(User::class)
+    // 	    ->findAllUsers();
 
-    	$this->assertCount($resultExpected, $users);
-    }
+    // 	$this->assertCount($resultExpected, $users);
+    // }
     
     /**
      * Recherche si le nomnre de users correspond à un nombre attendu
      * A utiliser avec les loadfixtures
      */
-	public function testListUsers()
-    {
-        // Commande : ./vendor/bin/simple-phpunit --filter=testListUsers
+	// public function testListUsers()
+ //    {
+ //        // Commande : ./vendor/bin/simple-phpunit --filter=testListUsers
 
-        $resultExpected = 20;
+ //        $resultExpected = 20;
 
-    	$users = $this->entityManager
-    	    ->getRepository(User::class)
-    	    ->findAllOrderedByName();
+ //    	$users = $this->entityManager
+ //    	    ->getRepository(User::class)
+ //    	    ->findAllOrderedByName();
 
-    	$this->assertCount($resultExpected, $users);
+ //    	$this->assertCount($resultExpected, $users);
 
-    }
+ //    }
 
-    public function testLastUserIsTitus()
-    {
-    	// Commande : ./vendor/bin/simple-phpunit --filter=testLastUserIsTitus
+ //    public function testLastUserIsTitus()
+ //    {
+ //    	// Commande : ./vendor/bin/simple-phpunit --filter=testLastUserIsTitus
     	
-    	$resultExpected = 'Titus';
+ //    	$resultExpected = 'Titus';
 
-		$users = $this->entityManager
-		    ->getRepository(User::class)
-		    ->findLastUser($resultExpected);
+	// 	$users = $this->entityManager
+	// 	    ->getRepository(User::class)
+	// 	    ->findLastUser($resultExpected);
 
-		$this->assertEquals($resultExpected, $users);
-    }
+	// 	$this->assertEquals($resultExpected, $users);
+ //    }
 
-    public function testAdminTestNomIsPresent()
-    {
-        // Commande : ./vendor/bin/simple-phpunit --filter=testAdminTotoIsPresent
+ //    public function testAdminTestNomIsPresent()
+ //    {
+ //        // Commande : ./vendor/bin/simple-phpunit --filter=testAdminTotoIsPresent
        
-        $users = $this->entityManager
-            ->getRepository(User::class)
-            ->verifyAdminTotoIsRegistered("user10", "user10@gmail.com");
+ //        $users = $this->entityManager
+ //            ->getRepository(User::class)
+ //            ->verifyAdminTotoIsRegistered("user10", "user10@gmail.com");
 
-        $this->assertCount(1, $users);
-    }
+ //        $this->assertCount(1, $users);
+ //    }
     
 	/**
 	 * {@inheritDoc}
 	 */
-	protected function tearDown()
-	{
-		parent::tearDown();
+	// protected function tearDown()
+	// {
+	// 	parent::tearDown();
 
-		$this->entityManager->close();
-		$this->entityManager = null;  //empêche les fuites mémoire
+	// 	$this->entityManager->close();
+	// 	$this->entityManager = null;  //empêche les fuites mémoire
 
-	}
+	// }
 	
 }
