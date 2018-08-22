13af426a5d653ce5b8f085980d6364b608c39b81
Khalid HAFID-MEDHEB <khalid.hafid-medheb@integragen.com>
2018-08-22 11:11:38 +0200

    test

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -20,19 +20,19 @@ class UserRepositoryTest extends KernelTestCase
 	/**
 	 * @var \Doctrine\ORM\EntityManager
 	 */
-	// private $entityManager;
+	private $entityManager;
 
 	/**
 	 * {@inheritDoc}
 	 */
-	// protected function setUp()
-	// {
-	// 	$kernel = self::bootKernel();
+	protected function setUp()
+	{
+		$kernel = self::bootKernel();
 
-	// 	$this->entityManager = $kernel->getContainer()
-	// 	    ->get('doctrine')
-	// 	    ->getManager();
-	// }
+		$this->entityManager = $kernel->getContainer()
+		    ->get('doctrine')
+		    ->getManager();
+	}
 
 	/**
 	 * Recherche si user10 existe dans users
@@ -55,18 +55,18 @@ class UserRepositoryTest extends KernelTestCase
      * Recherche si le nomnre de users correspond à un nombre attendu
      * A utiliser avec les loadfixtures
      */
-    // public function testFindAllUsers()
-    // {
-    // 	// Commande : ./vendor/bin/simple-phpunit --filter=testFindAllUsers
+    public function testFindAllUsers()
+    {
+    	// Commande : ./vendor/bin/simple-phpunit --filter=testFindAllUsers
     	
-    // 	$resultExpected = 9;
+    	$resultExpected = 20;
 
-    // 	$users = $this->entityManager
-    // 	    ->getRepository(User::class)
-    // 	    ->findAllUsers();
+    	$users = $this->entityManager
+    	    ->getRepository(User::class)
+    	    ->findAllUsers();
 
-    // 	$this->assertCount($resultExpected, $users);
-    // }
+    	$this->assertCount($resultExpected, $users);
+    }
     
     /**
      * Recherche si le nomnre de users correspond à un nombre attendu
